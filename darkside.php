3bf45b72b19fc404e60fd11c728a1caed50973af
mark172 <mark172@me.com>
2017-07-22 15:44:20 -0700

    added dark side car

---
+++ darkside.php
@@ -0,0 +1,44 @@
+<?php include 'header.php' ?>
+
+  <div class="offset"></div>
+  <div class="light-wrapper page-title">
+    <div class="container inner">
+      <h1 class="pull-left">Dark Side of the Rainbow</h1>
+    </div>
+  </div>
+  <div class="dark-wrapper">
+    <div class="container inner">
+      <div class="row">
+        <div class="col-sm-8">
+          <div class="owl-slider-wrapper">
+            <div class="owl-portfolio-slider owl-carousel">
+              <div class="item"> <img src="style/images/darkSide1.jpg" alt="" /> </div>
+              <div class="item"> <img src="style/images/darkSide2.jpg" alt="" /> </div>
+              <div class="item"> <img src="style/images/darkSide3.jpg" alt="" /> </div>
+            </div>
+            <div class="owl-custom-nav"> <a class="slider-prev"></a> <a class="slider-next"></a> </div>
+          </div>
+        </div>
+        <!-- /.col-sm-8 -->
+        <div class="col-sm-4 lp30">
+          <h3>Car Details</h3>
+          <p> 67 Fastback flagship restomod project will be complete with 347 stroker, 5-speed manual with overdrive, power windows and high end audio.  Shelby fiberglass front end and rear, but selectively chosen to not look like a clone!  Stay tuned!</p>
+          <p><strong>Update:</strong> She's getting final body work and paint over the next 2-3 weeks!</p>
+          <div class="divide5"></div>
+          <ul class="item-details">
+            <li><span>Year:</span> 1967</li>
+            <li><span>Code name:</span> Dark Side of the Rainbow</li>
+            <li><span>Model:</span> Fastback</li>
+            <li><span>Price:</span> TBD</li>
+          </ul>
+        </div>
+        <!-- /.col-sm-4 --> 
+      </div>
+      <!-- /.row --> 
+      
+    </div>
+    <!-- /.container --> 
+  </div>
+  <!-- /.dark-wrapper -->
+<?php include_once("analyticstracking.php") ?>
+<?php include 'footer.php' ?>
